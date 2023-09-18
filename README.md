2a0d52fe3c1829eb25a14dc5c376b9468f5ec5bb
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-09-18 14:39:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# FAQ Accordion Card Solution
+
+This is a solution to the [FAQ accordion card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+# Built with
+
+- HTML
+- CSS
+- JavaScript
---
+++ script.js
@@ -0,0 +1,24 @@
+const acc = document.querySelectorAll(".accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    const isSelected = this.classList.contains("selected");
+    
+    // Close all panels
+    for (let j = 0; j < acc.length; j++ ) {
+      acc[j].classList.remove("selected");
+      acc[j].firstElementChild.classList.remove("arrow-up");
+      acc[j].nextElementSibling.style.maxHeight = null;
+    }
+    
+    // Open selected panel
+    if (!isSelected) {
+      this.classList.add("selected");
+      this.firstElementChild.classList.add("arrow-up");
+      
+      const panel = this.nextElementSibling;
+      panel.style.maxHeight = panel.scrollHeight + "px";      
+    }
+  });
+}
